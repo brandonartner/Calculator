0051d04a714026231cf8eed47766ea5e0fa93ec9
Brandon Artner <brandon.artner739@myci.csuci.edu>
2016-02-05 21:47:42 -0800

    Added names

--- src/simplecalculator/calculator.java
+++ src/simplecalculator/calculator.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Authors: Brandon Artner & Michael Bennett
+ * Assignment: Lab 2
+ * Date: 2/7/16
  */
 package simplecalculator;
 
